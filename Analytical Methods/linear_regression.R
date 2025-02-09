899f5c1db92a7e170af81b342b50efb50c45ad4f
Joe <josephb.1993@gmail.com>
2025-02-09 17:18:13 +0000

    Update linear_regression.R

--- Analytical Methods/linear_regression.R
+++ Analytical Methods/linear_regression.R
@@ -1,9 +1,24 @@
-# Sample data
-X <- c(1, 2, 3, 4, 5)
-y <- c(2, 4, 5, 4, 5)
+# 1. Check for linearity using a scatter plot
+plot(data$x, data$y, main = "Scatter Plot of x vs y", xlab = "x", ylab = "y")
+abline(lm(y ~ x, data = data), col = "red")  # Add regression line
 
-# Fit the model 
-model <- lm(y ~ X) 
+# 2. Fit the linear regression model
+model <- lm(y ~ x, data = data)
 
-# Print the summary of the regression 
+# 3. Check for normality of residuals
+par(mfrow = c(1, 2))  # Set plotting area to show two plots
+hist(resid(model), main = "Histogram of Residuals", xlab = "Residuals", col = "lightblue")
+qqnorm(resid(model))
+qqline(resid(model), col = "red")  # Add a reference line
+
+# 4. Check for homoscedasticity (constant variance)
+plot(fitted(model), resid(model), main = "Residuals vs Fitted Values", xlab = "Fitted Values", ylab = "Residuals")
+abline(h = 0, col = "red")
+
+# 5. Check for multicollinearity using Variance Inflation Factor (VIF)
+# Only applies when multiple predictors are present
+vif_values <- vif(model)
+print(vif_values)
+
+# 6. Summary of the regression model
 summary(model)
