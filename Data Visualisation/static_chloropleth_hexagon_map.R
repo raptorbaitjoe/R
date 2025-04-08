d7452c3691f0a1e60f3b6c3c6a617b3d11b6f902
Joe <josephb.1993@gmail.com>
2025-04-08 19:30:15 +0100

    Update static_chloropleth_hexagon_map.R

--- Data Visualisation/static_chloropleth_hexagon_map.R
+++ Data Visualisation/static_chloropleth_hexagon_map.R
@@ -1,59 +1,62 @@
-install.packages(c("sf", "ggmap", "ggplot2", "dplyr", "viridis", "rmapshaper"))
+####                    Load Packages and filepaths                         ####
+usePackage <- function(p)
+{ if (!is.element(p, installed.packages()[,1]))
+  install.packsges(p, dep = TRUE)
+  require(p, character.only = TRUE) }
 
-library(sf)
-library(ggmap)
-library(ggplot2)
-library(dplyr)
-library(viridis)
-library(rmapshaper)
-
-
-
-# Load local shapefile
+usePackage("dplyr")
+usePackage("sf")
+usePackage("ggmap")
+usePackage("ggplot2")
+usePackage("viridis")
+usePackage("rmapshaper")
 
 zip_path <- "C:/Users/Joseph/Downloads/Countries_December_2024_Boundaries_UK_BFC_6983126662299524946.zip"
+                                                                                # define zip location
+unzip_dir <- tempdir()                                                          # set path for unzip location
 
-# Unzip to a temp directory (or choose your own)
-unzip_dir <- tempdir()                       # set path for unzip location
+
+####                    Load and filter shapefile                           ####
+
 utils::unzip(zip_path, exdir = unzip_dir)    # unzip function - take from zip path, place in unzip path
 
-# Find the .shp file (in case the zip has multiple files)
-shp_file <- base::list.files(unzip_dir, pattern = "\\.shp$", full.names = TRUE)[1]
 
-# Read the shapefile
-england_shapefile <- sf::st_read(shp_file) %>%
-  filter(CTRY24CD == "E92000001") %>%     # Filter to just England
-  st_transform(4326)                      # WGS84 for mapping
+shp_file <- base::list.files(unzip_dir, pattern = "\\.shp$", full.names = TRUE)[1] # Find the .shp file (in case the zip has multiple files)
 
+england_shapefile <- sf::st_read(shp_file) %>%                                  # Read the shapefile
+                     dplyr::filter(CTRY24CD == "E92000001") %>%                 # Filter to just England
+                     sf::st_transform(4326)                                     # WGS84 for mapping
 
 
-england_simple <- ms_simplify(england_shapefile, keep = 0.05)
+england_simple <- mapshaper::ms_simplify(england_shapefile, keep = 0.05)        # Simplify the geometry
 
-remove(england_shapefile)
+####                    Create and populate hex grid                        ####
 
-hex_grid <- st_make_grid(england_simple, cellsize = 0.3, square = FALSE) %>%
-  st_as_sf() %>%
-  st_filter(england_simple)
+hex_grid <- sf::st_make_grid(england_simple, cellsize = 0.3, square = FALSE) %>%   # Create hex grid of shapefile's bounding box
+  sf::st_as_sf() %>%                                                               # Convert hex grid to shapefile
+  sf::st_filter(england_simple)                                                    # Spatial filter - returns only hexes that insterest england_simple
 
 
-# Add dummy values
-set.seed(42)
-hex_grid$val <- runif(nrow(hex_grid), 0, 100)
+set.seed(42)                                                                    # Set the random seed                  
+hex_grid$val <- runif(nrow(hex_grid), 0, 100)                                   # Creates value column and populates with random numbers
+
+
+####                    Create map                                          ####
 
 map <- ggplot() +
-  geom_sf(data = england_shapefile, fill = "grey90", color = "white") +  # base map
-  geom_sf(data = hex_grid, aes(fill = val), color = "white", size = 0.1, alpha = 0.8) +
-  scale_fill_viridis_c(name = "Value", option = "plasma") +
-  coord_sf(crs = st_crs(4326)) +
-  theme_minimal() +
-  labs(
+  geom_sf(data = england_shapefile, fill = "grey90", color = "black") +                 # Load england_shapefile as basemap 
+  geom_sf(data = hex_grid, aes(fill = val), color = "white", size = 0.1, alpha = 0.8) + # load hex grid and set visuals
+  scale_fill_viridis_c(name = "Value", option = "plasma") +                             # set colour scale for hex grid to viridis
+  coord_sf(crs = st_crs(4326)) +                                                        # set the coordinate system (4326 = WGS84)
+  theme_minimal() +                                                                     # Theme
+  labs(                                                                                 # Labels
     title = "Hexagon Choropleth Map over England",
     caption = "Based on local shapefile"
   ) +
-  theme(
+  theme(                                                                                # Legend & axes
     legend.position = "right",
     axis.text = element_blank(),
     axis.ticks = element_blank()
   )
 
-map
+map                                                                             # Print map
