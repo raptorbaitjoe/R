a075a71e6381bcf5f9ccd76002f1693bc32a9f34
Joe <josephb.1993@gmail.com>
2025-08-13 16:58:05 +0100

    Update and rename intermediate_R.R to intermediate_R_chapter_one.R

---
+++ Datacamp/intermediate_R_chapter_one.R
@@ -0,0 +1,229 @@
+## Relational Operators
+# Allow us to see how variables relate to each other
+
+# Equality ==
+# Inequality !=
+# < and >
+# <= and >=
+
+# Relational Operators and Vectors
+# For a vector, a relational operator will run on each individual variable within it
+# You can also compare vectors, which compares each value to the value in the same position in the other vector
+
+## Exercise
+# Comparison of logicals
+TRUE == FALSE
+
+# Comparison of numerics
+(-6*14) != (17-101)
+
+# Comparison of character strings
+"useR" == "user"
+
+# Compare a logical with a numeric
+TRUE == 1
+
+# Comparison of numerics
+(-6*5+2) >= (-10+1)
+
+# Comparison of character strings
+"raining" <= "raining dogs"
+
+# Comparison of logicals
+TRUE > FALSE
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Popular days
+linkedin > 15
+
+# Quiet days
+linkedin <= 5
+
+# LinkedIn more popular than Facebook
+linkedin > facebook
+
+# The social data has been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+views <- matrix(c(linkedin, facebook), nrow = 2, byrow = TRUE)
+
+# When does views equal 13?
+views == 13
+
+# When is views less than or equal to 14?
+views <= 14
+
+## Logical Operators
+
+# AND & - only if operators are both true
+# OR | either operator is true
+# NOT ! if operator is false (writen before)
+
+## Exercise
+# The linkedin and last variable are already defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+last <- tail(linkedin, 1)
+
+# Is last under 5 or above 10?
+last < 5 | last > 10
+
+# Is last between 15 (exclusive) and 20 (inclusive)?
+last > 15 & last <= 20
+
+# The social data (linkedin, facebook, views) has been created for you
+
+# linkedin exceeds 10 but facebook below 10
+linkedin > 10 & facebook < 10
+
+# When were one or both visited at least 12 times?
+linkedin >= 12 | facebook >= 12
+
+# When is views between 11 (exclusive) and 14 (inclusive)?
+views > 11 & views <= 14
+
+# Subsequent ! can cancel each other
+# ! = not/isn't
+# !! = not not/is
+# !!! = not not not/isn't
+
+# li_df is pre-loaded in your workspace
+
+## Blend it all together
+
+# Select the second column, named day2, from li_df: second
+second <- li_df$day2
+
+# Build a logical vector, TRUE if value in second is extreme: extremes
+extremes <- second > 25|second < 5
+
+# Count the number of TRUEs in extremes
+sum(extremes == TRUE)
+
+## Conditional Statements - if, else if, else
+
+# If
+if(x < 0) {
+  print("x is a negative number")
+}
+
+# Else
+if (condition) {
+  expr1
+} else {
+  expr2
+}
+
+
+if(x < 0) {
+  print("x is a negative number")
+} else {
+  print"x is either a positive number or zero")
+}
+
+# else if
+
+if(condition1) {
+  expr1
+} else if(condition2) {
+  expr2
+} else {
+  expr3
+}
+
+if(x < 0) {
+  print("x is a negative number")
+} else if(x == 0) {
+    print("x is zero")
+} else {
+  print"x is either a positive number")
+}
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Examine the if statement for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}
+
+# Write the if statement for num_views
+if (num_views > 15) {
+  print("You are popular!")
+}
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else {
+  print("Try to be more visible!")
+}
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  # Add code to print correct string when condition is TRUE
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  # Add code to print correct string when condition is TRUE
+  print("Your number of views is average")
+} else {
+  print("Try to be more visible!")
+}
+
+# Else if 2.0
+
+if (number < 10) {
+  if (number < 5) {
+    result <- "extra small"
+  } else {
+    result <- "small"
+  }
+} else if (number < 100) {
+  result <- "medium"
+} else {
+  result <- "large"
+
+ # Variables related to your last day of recordings
+li <- 15
+fb <- 9
+
+# Code the control-flow construct
+if (li >= 15 & fb >= 15) {
+  sms <- 2 * (li + fb)
+} else if (li < 10 & fb < 10) {
+  sms <- 0.5 * (li + fb)
+} else {
+  sms <- (li + fb)
+}
+
+# Print the resulting sms to the console
+print(sms) 
+}
+print(result)
