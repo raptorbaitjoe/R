c136bc6cc2205e59796005b147b4b3eeaf8007a9
Joe <josephb.1993@gmail.com>
2025-08-13 17:24:01 +0100

    Create intermediate_R_chapter_two.R

---
+++ Datacamp/intermediate_R_chapter_two.R
@@ -0,0 +1,165 @@
+## While loop
+
+while(condition){
+  expr 
+}
+
+while(ctr <= 7) {
+  print(paste("ctr is set to", ctr))
+  ctr <- ctr + 1
+}
+
+# break statement - breaks out of while loop
+
+while(ctr <= 7) {
+  if(ctr %% 5 == 0) {
+    break
+  }
+  print(paste("ctr is set to", ctr))
+  ctr <- ctr + 1
+}
+
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  print("Slow down!")
+  speed <- speed - 7
+}
+
+# Print out the speed variable
+speed
+
+# Initialize the speed variable
+speed <- 64
+
+# Extend/adapt the while loop
+while (speed > 30) {
+  print(paste("Your speed is", speed))
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed - 6
+  }
+}
+
+# Initialize the speed variable
+speed <- 88
+
+while (speed > 30) {
+  print(paste("Your speed is", speed))
+  
+  # Break the while loop when speed exceeds 80
+  if (speed > 80) {
+    break
+  }
+  
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed - 6
+  }
+}
+
+# Initialize i as 1 
+i <- 1
+
+# Code the while loop
+while (i <= 10) {
+  print(3 * i)
+  if ((3 * i) %% 8 == 0) {
+    break
+  }
+  i <- i + 1
+}
+
+## For loop
+
+for(var in seq) {
+  expr
+}
+
+for(city in cities) { # R looks for cities and understands the vector of cities, then realises we want to call each element city
+  print(city)
+}
+
+# break statements - stops the loop
+
+for(city in cities) {
+  if(nchar(city) == 6) { # if city has six characters
+    break
+  }
+  print(city)
+}
+
+# next statements - skips to the next one
+
+for(city in cities) {
+  if(nchar(city) == 6) { # if city has six characters
+    next
+  }
+  print(city)
+}
+
+# for loop: v2 - allows access to index
+
+for(i in 1:length(cities)) {
+  print(paste(cities[i], "is on position",
+              i, "in the cities vector."))
+}
+
+# Loop over a vector
+
+primes <- c(2, 3, 5, 7, 11, 13)
+
+# loop version 1
+for (p in primes) {
+  print(p)
+}
+
+# loop version 2
+for (i in 1:length(primes)) {
+  print(primes[i])
+}
+
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for(link in linkedin) {
+    print(link)
+}
+
+# Loop version 2
+for(i in 1:length(linkedin)) {
+    print(linkedin[i])
+}
+
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for(point in nyc) {
+    print(point)
+}
+
+# Loop version 2
+for(i in 1:length(nyc)) {
+    print(nyc[[i]])
+}
+
+# The tic-tac-toe matrix ttt has already been defined for you
+
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
+  }
+}
