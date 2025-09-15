49b22fbf2bb2c7ca2d783ab56e2ddbfb191e62a9
Joe <josephb.1993@gmail.com>
2025-09-15 20:32:11 +0100

    Update and rename intermediate_R_chapter_two.R to intermediate_R_chapter_two_loops.R

--- Datacamp/intermediate_R_chapter_two_loops.R
+++ Datacamp/intermediate_R_chapter_two_loops.R
@@ -163,3 +163,50 @@ for (i in 1:nrow(ttt)) {
     print(paste("On row", i, "and column", j, "the board contains", ttt[i,j]))
   }
 }
+
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Adapt/extend the for loop
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  
+  # Add if statement with break
+  if (li > 16) {
+    print("This is ridiculous, I'm outta here!")
+    break
+  }
+  
+  # Add if statement with next
+  if (li < 5) {
+    print("This is too embarrassing!")
+    next
+  }
+  
+  print(li)
+}
+
+
+
+# Pre-defined variables
+rquote <- "r's internals are irrefutably intriguing"
+chars <- strsplit(rquote, split = "")[[1]]
+
+# Initialize rcount
+rcount <- 0
+
+# Finish the for loop
+for (char in chars) {
+  if (char == "r") {
+      rcount <- rcount + 1
+  } 
+  if (char == "u") {
+      break 
+  }
+# Print out rcount
+print(rcount)
+  }
