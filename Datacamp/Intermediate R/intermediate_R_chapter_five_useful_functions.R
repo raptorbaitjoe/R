ba3351e251558f1e80fafd69a3d156f23a2b960e
Joe <josephb.1993@gmail.com>
2026-06-08 19:13:49 +0100

    Update intermediate_R_chapter_five_useful_functions.R

--- Datacamp/Intermediate R/intermediate_R_chapter_five_useful_functions.R
+++ Datacamp/Intermediate R/intermediate_R_chapter_five_useful_functions.R
@@ -55,3 +55,164 @@ social_vec
 
 # Fix me
 rep(seq(1, 7, by = 2), times = 7)
+
+## grepl & grep
+# grepl(), which returns TRUE when a pattern is found in the corresponding character string.
+# grep(), which returns a vector of indices of the character strings that contains the pattern.
+
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org",
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+
+# Use grepl() to match for "edu"
+grepl("edu", emails)
+
+# Use grep() to match for "edu", save result to hits
+hits <- grep("edu", emails)
+
+# Subset emails using hits
+emails[hits]
+
+# You can use the caret, ^, and the dollar sign, $ to match the content located in the start and end of a string, respectively.
+# \\.edu$, to match the ".edu" part of the email at the end of the string. The \\ part escapes the dot: it tells R that you want to use the . as an actual character.
+
+# Use grepl() to match for .edu addresses more robustly
+grepl("@.*\\.edu$", emails)
+
+# Use grep() to match for .edu addresses more robustly, save result to hits
+hits <- grep("@.*\\.edu$", emails)
+
+# Subset emails using hits
+emails[hits]
+
+## sub & gsub
+# sub() only replaces the first match, whereas gsub() replaces all matches.
+
+# Use sub() to convert the email domains to datacamp.edu
+sub("@.*\\.edu$", "@datacamp.edu", emails)
+
+# .*: A usual suspect! It can be read as "any character that is matched zero or more times".
+# \\s: Match a space. The "s" is normally a character, escaping it (\\) makes it a metacharacter.
+# [0-9]+: Match the numbers 0 to 9, at least once (+).
+# ([0-9]+): The parentheses are used to make parts of the matching string available to define the replacement. The \\1 in the replacement argument of sub() gets set to the string that is captured by the regular expression [0-9]+.
+
+## Times and Dates
+
+# today <- Sys.Date()
+# my_date <- as.Date("2026-06-08", format = "%Y-%d-%m")
+# my_time <- as.POSIXct("1971-05-14 11:25:15")
+# packages: lubridate, zz, xts
+
+# Get the current date: today
+today <- Sys.Date()
+
+# See what today looks like under the hood
+unclass(today)
+
+# Get the current time: now
+now <- Sys.time()
+
+# See what now looks like under the hood
+unclass(now)
+
+## Create and format dates
+#    %Y: 4-digit year (1982)
+#    %y: 2-digit year (82)
+#    %m: 2-digit month (01)
+#    %d: 2-digit day of the month (13)
+#    %A: weekday (Wednesday)
+#    %a: abbreviated weekday (Wed)
+#    %B: month (January)
+#    %b: abbreviated month (Jan)
+
+# Definition of character strings representing dates
+str1 <- "May 23, '96"
+str2 <- "2012-03-15"
+str3 <- "30/January/2006"
+
+# Convert the strings to dates: date1, date2, date3
+date1 <- as.Date(str1, format = "%b %d, '%y")
+date2 <- as.Date(str2, format = "%Y-%m-%d")
+date3 <- as.Date(str3, format = "%d/%B/%Y")
+
+# Convert dates to formatted strings
+format(date1, "%A")
+format(date2, "%d")
+format(date3, "%b %Y")
+
+## Create and format times
+# %H: hours as a decimal number (00-23)
+# %I: hours as a decimal number (01-12)
+# %M: minutes as a decimal number
+# %S: seconds as a decimal number
+# %T: shorthand notation for the typical format %H:%M:%S
+# %p: AM/PM indicator
+
+# Definition of character strings representing times
+str1 <- "May 23, '96 hours:23 minutes:01 seconds:45"
+str2 <- "2012-3-12 14:23:08"
+
+# Convert the strings to POSIXct objects: time1, time2
+time1 <- as.POSIXct(str1, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")
+time2 <- as.POSIXct(str2)
+
+# Convert times to formatted strings
+format(time1, "%M")
+format(time2, "%I:%M %p")
+
+## Calculations with dates
+
+# today <- Sys.Date()
+# today + 1
+# today - 1
+
+# as.Date("2015-03-12") - as.Date("2015-02-27")
+
+# day1, day2, day3, day4 and day5 are already available in the workspace
+
+# Difference between last and first pizza day
+day5 - day1
+
+# Create vector pizza
+pizza <- c(day1, day2, day3, day4, day5)
+
+# Create differences between consecutive pizza days: day_diff
+day_diff <- diff(pizza)
+
+# Average period between two consecutive pizza days
+mean(day_diff)
+
+## Calculations with Times
+
+# now <- Sys.time()
+# now + 3600          # add an hour
+# now - 3600 * 24     # subtract a day
+
+# Adding or subtracting time objects is also straightforward:
+
+# birth <- as.POSIXct("1879-03-14 14:37:23")
+# death <- as.POSIXct("1955-04-18 03:47:12")
+# einstein <- death - birth
+# einstein
+
+# login and logout are already defined in the workspace
+# Calculate the difference between login and logout: time_online
+time_online <- logout - login
+
+# Inspect the variable time_online
+time_online
+
+# Calculate the total time online
+sum(time_online)
+
+# Calculate the average time online
+mean(time_online)
+
+# Convert astro to vector of Date objects: astro_dates
+astro_dates <- as.Date(astro, format = "%d-%b-%Y")
+
+# Convert meteo to vector of Date objects: meteo_dates
+meteo_dates <- as.Date(meteo, format = "%B %d, %y")
+
+# Calculate the maximum absolute difference between astro_dates and meteo_dates
+max(abs(meteo_dates - astro_dates))
